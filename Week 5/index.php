6213108df06424440b085fa5bee48c8fc2823de1
ambika900 <53795629+ambika900@users.noreply.github.com>
2020-06-02 02:57:01 +0530

    Add files via upload

---
+++ Week 5/index.php
@@ -0,0 +1,16 @@
+<title>Ambika Patidar PHP</title>
+<h1>Ambika Patidar PHP</h1>
+<p><?php print hash('sha256', 'Ambika Patidar'); ?></p>
+<pre>ASCII ART:
+           ***
+          *****
+         **   **
+        **     **
+       ***********
+      *************
+     **           **
+    **             **
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br>
+<a href="fail.php">Click here to cause a traceback</a>
